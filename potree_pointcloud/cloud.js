2be67a5e8d7d6a3769699dfd440613eab7eb6031
Fábio Quintas Gonçalves <goncalves.fabio.inc@gmail.com>
2019-04-23 19:31:53 +0100

    Adicionado potree_pointcloud

---
+++ potree_pointcloud/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "points": 7438054,
+    "boundingBox": {
+        "lx": 554031.7871814572,
+        "ly": 4505755.330879385,
+        "lz": -18.65797028691101,
+        "ux": 554365.2833707432,
+        "uy": 4506088.827068671,
+        "uz": 314.83821899909005
+    },
+    "tightBoundingBox": {
+        "lx": 554031.79,
+        "ly": 4505755.33,
+        "lz": -18.66,
+        "ux": 554351.93,
+        "uy": 4506088.83,
+        "uz": 52.32
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 2.3105294704437258,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
